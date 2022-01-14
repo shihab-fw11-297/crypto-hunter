4710d17c0efc5217e09c3ade66d9648325440f37
shihab-fw11-297 <88572629+shihab-fw11-297@users.noreply.github.com>
2022-01-14 15:02:17 +0530

    home page

---
+++ crypto-hunter/src/Pages/CoinPage.js
@@ -0,0 +1,10 @@
+
+const CoinPage = () => {
+    return (
+        <div className='header'>
+            <span className='title'>Word CoinPage</span>
+        </div>
+    )
+}
+
+export default CoinPage
---
+++ crypto-hunter/src/Pages/HomePage.js
@@ -0,0 +1,11 @@
+import Banner from '../components/Banner/Banner.js'
+
+const HomePage = () => {
+    return (
+        <>
+           <Banner/>
+        </>
+    )
+}
+
+export default HomePage
